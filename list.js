bac52039504c5dab0cf9c9001ea66230f96a21ba
byjo <buyongjo@gmail.com>
2015-08-01 11:18:59 +0900

    apply prototype pattern

--- list.js
+++ list.js
@@ -1,49 +1,56 @@
-var count = 0;
-var template;
+(function() {
 
-(function () {
-	document.addEventListener("DOMContentLoaded", function() {
-		var btnAdd = document.getElementById("add");
-		var btnRemove = document.getElementById("remove");
-
-		btnAdd.addEventListener("click", addList);
-		btnRemove.addEventListener("click", removeList);
-
-		template = document.getElementById("list").innerHTML;
-		document.getElementById("list").innerHTML = "";
-	});
-
-	function addList() {
-		var node = document.createElement("li");
-		var content = document.getElementById("listContent").value;
-		document.getElementById("listContent").value = "";
-
-		var temp = template.replace("{{count}}", count++);
-		node.innerHTML = temp.replace("{{content}}", content);
-
-
-		//ul.appendChild(node);
-		document.getElementById("list").appendChild(node);
+	var count = 0;
+	
+	function List() {
 	}
 
-	function removeList() {
-		var checkedList = [];
-		var checkedInput = document.getElementsByTagName("input");
-		// checkedList = checkedInput.map(function(item) {
-		// 	return item.parentNode;
-		// });
-		for (var i=0; i<checkedInput.length; i++) {
-			if (checkedInput[i].checked)
-				//checkedInput[i].parentNode.removeChild(checkedInput[i]);
-				checkedList.push(checkedInput[i].parentNode);
-		}
+	// List.prototype.init = function(ulEle) {
+	// 	console.log(this);
+	// 	this.ulList = ulEle;
+	// 	this.template = this.ulList.innerHTML;
+	// 	this.ulList.innerHTML = "";
+	// }
 
-		var list = document.getElementById("list");
-		for (var i=0; i<checkedList.length; i++) {
-			list.removeChild(checkedList[i]);
-		}
-		
-		//document.getElementById("list").removeChild(checkedList);
+	// List.prototype.add = function() {
+	// 	console.log(this);
+	// 	var node = document.createElement("li");
+	// 	List.ulList.appendChild(node);
+	// }
+
+	// -init
+	// input : ul element 
+	// output : x
+	List.prototype.init = function(ulEle, btnAdd, btnDel) {
+		this.ulList = ulEle;
+		this.template = this.ulList.innerHTML;
+		// template내의 {{ }} 안의 애들을 key로 하는 맵 생성 
+		this.context;
+		this.ulList.innerHTML = "";
+
+		btnAdd.addEventListener("click", this.add.bind(this));
+	}
+
+	// -add
+	// input :
+	// output :
+	List.prototype.add = function() {
+		var node = document.createElement("li");
+		this._setContents(node);
+		this.ulList.appendChild(node);
+	}
+
+	// -remove 
+	// input :
+	// output :
+	List.prototype.remove = function() {
+
+	}
+
+	List.prototype._setContents = function() {
+		// context update
+		// template 에 적용 
 	}
 
 })();
+
